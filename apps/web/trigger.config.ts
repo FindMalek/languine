5cb9022183b84751e87c261f69ed704d945553a2
FindMalek <hi@findmalek.com>
2026-04-28 08:37:48 +0100

    fix: resolved 'projectId'

--- apps/web/trigger.config.ts
+++ apps/web/trigger.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
-  project: "proj_rtlqllxxclkoghkeuwve",
+  project: "proj_lpwuneulrklyulyuryew",
   runtime: "node",
   logLevel: "log",
   maxDuration: 3600,
